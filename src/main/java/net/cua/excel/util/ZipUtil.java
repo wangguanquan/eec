f214328172784385757e228ed9bc9c0fa9724cb6
wangguanquan <guanquan.wang@yandex.com>
2019-02-28 18:48:14 +0800

    Add Apache License Version 2.0 into eec

--- src/main/java/net/cua/excel/util/ZipUtil.java
+++ src/main/java/net/cua/excel/util/ZipUtil.java
@@ -1,9 +1,19 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package net.cua.excel.util;
 
-/**
- * zip util
- * Created by guanquan.wang at 2017/10/13.
- */
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -15,6 +25,10 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.zip.*;
 
+/**
+ * zip util
+ * Created by guanquan.wang at 2017/10/13.
+ */
 public class ZipUtil {
     private static final String suffix = ".zip";
     private ZipUtil() {}
