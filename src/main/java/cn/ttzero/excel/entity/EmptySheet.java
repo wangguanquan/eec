e1db377d7f84cfa78fd6e48a100d34f9cb3e1db1
wangguanquan <colvin@126.com>
2019-04-29 22:36:32 +0800

    Upgrade EmptySheet

--- src/main/java/cn/ttzero/excel/entity/EmptySheet.java
+++ src/main/java/cn/ttzero/excel/entity/EmptySheet.java
@@ -16,79 +16,50 @@
 
 package cn.ttzero.excel.entity;
 
-import cn.ttzero.excel.util.ExtBufferedWriter;
-import cn.ttzero.excel.util.FileUtil;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-
 /**
  * Created by guanquan.wang at 2018-01-29 16:05
  */
 public class EmptySheet extends Sheet {
-    public EmptySheet(Workbook workbook, String name, Column ... columns) {
-        super(workbook, name, columns);
+
+    /**
+     * Constructor worksheet
+     */
+    public EmptySheet() {
+        super();
     }
 
-    public EmptySheet(Workbook workbook, String name, WaterMark waterMark, Column ... columns) {
-        super(workbook, name, waterMark, columns);
+    /**
+     * Constructor worksheet
+     * @param name the worksheet name
+     */
+    public EmptySheet(String name) {
+        super(name);
     }
 
+    /**
+     * Constructor worksheet
+     * @param name the worksheet name
+     * @param columns the header info
+     */
+    public EmptySheet(String name, final Column[] columns) {
+        super(name, columns);
+    }
 
-//    @Override
-//    public void writeTo(Path xl) throws IOException {
-//        Path worksheets = xl.resolve("worksheets");
-//        if (!Files.exists(worksheets)) {
-//            FileUtil.mkdir(worksheets);
-//        }
-//        String name = getFileName();
-//        workbook.what("0010", getName());
-//
-//
-//        File sheetFile = worksheets.resolve(name).toFile();
-//
-////        // write date
-////        try (ExtBufferedWriter bw = new ExtBufferedWriter(new OutputStreamWriter(new FileOutputStream(sheetFile), StandardCharsets.UTF_8))) {
-////            // Write header
-////            writeBefore(bw);
-////            // Main data
-////            // write ten empty rows
-////            for (int i = 0; i < 10; i++) {
-////                writeEmptyRow(bw);
-////            }
-////
-////            // Write foot
-////            writeAfter(bw);
-////
-////        } finally {
-////            close();
-////        }
-////
-////        // resize columns
-////        boolean resize = false;
-////        for  (Column hc : columns) {
-////            if (hc.getWidth() > 0.000001) {
-////                resize = true;
-////                break;
-////            }
-////        }
-////        if (getAutoSize() == 1 || resize) {
-////            autoColumnSize(sheetFile);
-////        }
-////
-////        // relationship
-////        relManager.write(worksheets, name);
-//    }
+    /**
+     * Constructor worksheet
+     * @param name the worksheet name
+     * @param waterMark the water mark
+     * @param columns the header info
+     */
+    public EmptySheet(String name, WaterMark waterMark, final Column[] columns) {
+        super(name, waterMark, columns);
+    }
 
+    /**
+     * Reset the row-block data
+     */
     @Override
-    public RowBlock nextBlock() {
-        return null;
-    }
+    protected void resetBlockData() { }
 
     /**
      * Returns total rows in this worksheet
