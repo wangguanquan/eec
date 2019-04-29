16730fb3e776198c9237034e93cf18bd3a3982c7
guanquan.wang <guanquan.wang@yandex.com>
2019-04-29 15:23:46 +0800

    Add ResultSet paging test case

--- src/test/java/cn/ttzero/excel/entity/ResultSetPagingTest.java
+++ src/test/java/cn/ttzero/excel/entity/ResultSetPagingTest.java
@@ -1,4 +1,6 @@
 /*
+ * Copyright (c) 2019, guanquan.wang@yandex.com All Rights Reserved.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -24,24 +26,26 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
- * Create by guanquan.wang at 2019-04-28 21:50
+ * Create by guanquan.wang at 2019-04-29 15:16
  */
-public class ResultSetSheetTest extends SQLWorkbookTest {
-    @Test public void testWrite() {
+public class ResultSetPagingTest extends SQLWorkbookTest {
+    @Test
+    public void testPaging() {
         try (Connection con = getConnection()) {
-            PreparedStatement ps = con.prepareStatement("select id, name, age from student order by age");
+            PreparedStatement ps = con.prepareStatement("select id, name, age from student");
             ResultSet rs = ps.executeQuery();
-            new Workbook("result set", author)
+            Workbook workbook = new Workbook("result set paging", author)
                 .watch(Print::println)
                 .setConnection(con)
                 .addSheet(rs
                     , new Sheet.Column("学号", int.class)
                     , new Sheet.Column("性名", String.class)
                     , new Sheet.Column("年龄", int.class)
-                )
+                );
+            workbook.setWorkbookWriter(new MyXMLWorkbookWriter(workbook))
                 .writeTo(defaultTestPath);
             ps.close();
-        } catch (SQLException|IOException e) {
+        } catch (SQLException |IOException e) {
             e.printStackTrace();
         }
     }
