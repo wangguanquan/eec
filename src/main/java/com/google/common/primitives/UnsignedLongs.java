3326557119d286a55226cd8fb72cd00ee10a9ea7
guanquan.wang <guanquan.wang@yandex.com>
2019-05-07 19:33:42 +0800

    Import google bloom filter

---
+++ src/main/java/com/google/common/primitives/UnsignedLongs.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (C) 2011 The Guava Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.google.common.primitives;
+
+/**
+ * Static utility methods pertaining to {@code long} primitives that interpret values as
+ * <i>unsigned</i> (that is, any negative value {@code x} is treated as the positive value {@code
+ * 2^64 + x}). The methods for which signedness is not an issue are in {@link Longs}, as well as
+ * signed versions of methods for which signedness is an issue.
+ *
+ * <p>In addition, this class provides several static methods for converting a {@code long} to a
+ * {@code String} and a {@code String} to a {@code long} that treat the {@code long} as an unsigned
+ * number.
+ *
+ * <p>Users of these utilities must be <i>extremely careful</i> not to mix up signed and unsigned
+ * {@code long} values. When possible, it is recommended that the {@link UnsignedLong} wrapper class
+ * be used, at a small efficiency penalty, to enforce the distinction in the type system.
+ *
+ * <p>See the Guava User Guide article on <a
+ * href="https://github.com/google/guava/wiki/PrimitivesExplained#unsigned-support">unsigned
+ * primitive utilities</a>.
+ *
+ * @author Louis Wasserman
+ * @author Brian Milch
+ * @author Colin Evans
+ * @since 10.0
+ */
+public final class UnsignedLongs {
+  private UnsignedLongs() {}
+
+  public static final long MAX_VALUE = -1L; // Equivalent to 2^64 - 1
+
+  /**
+   * A (self-inverse) bijection which converts the ordering on unsigned longs to the ordering on
+   * longs, that is, {@code a <= b} as unsigned longs if and only if {@code flip(a) <= flip(b)} as
+   * signed longs.
+   */
+  private static long flip(long a) {
+    return a ^ Long.MIN_VALUE;
+  }
+
+  /**
+   * Compares the two specified {@code long} values, treating them as unsigned values between {@code
+   * 0} and {@code 2^64 - 1} inclusive.
+   *
+   * <p><b>Java 8 users:</b> use {@link Long#compareUnsigned(long, long)} instead.
+   *
+   * @param a the first unsigned {@code long} to compare
+   * @param b the second unsigned {@code long} to compare
+   * @return a negative value if {@code a} is less than {@code b}; a positive value if {@code a} is
+   *     greater than {@code b}; or zero if they are equal
+   */
+  public static int compare(long a, long b) {
+    return Long.compare(flip(a), flip(b));
+  }
+
+}
