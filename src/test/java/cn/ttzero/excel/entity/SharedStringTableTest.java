2b16dd42c6f756679d8543091f610b965c82d1a8
guanquan.wang <guanquan.wang@yandex.com>
2019-05-08 19:37:50 +0800

    Add test case

---
+++ src/test/java/cn/ttzero/excel/entity/SharedStringTableTest.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2019, guanquan.wang@yandex.com All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cn.ttzero.excel.entity;
+
+import cn.ttzero.excel.Print;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+import static cn.ttzero.excel.Print.println;
+import static cn.ttzero.excel.entity.WorkbookTest.getRandomString;
+import static cn.ttzero.excel.Print.print;
+
+/**
+ * Create by guanquan.wang at 2019-05-08 17:04
+ */
+public class SharedStringTableTest {
+    private SharedStringTable sst;
+
+    @Before public void before() {
+        sst = new SharedStringTable();
+    }
+
+//    @After public void after() throws IOException {
+//        sst.close();
+//    }
+
+    @Test public void testPutChar() throws IOException {
+        int n = sst.push('a');
+
+        assert n == 0;
+        assert sst.size() == 1;
+
+        int index = sst.find('a');
+        assert index == 0;
+    }
+
+    @Test public void testPutString() throws IOException {
+        int n = sst.push("abc");
+        assert n == 1;
+        assert sst.size() == 2;
+
+        int index = sst.find("ab");
+        assert index == -1;
+
+        index = sst.find("abc");
+        assert index == 1;
+    }
+
+    @Test public void testPush() throws IOException {
+        for (int i = 0; i < 1_000; i++) {
+            sst.push(getRandomString());
+        }
+
+//        sst.forEach(Print::println);
+        int i = 0;
+        for (Iterator<String> it = sst.iterator(); it.hasNext();) {
+            print(i++);
+            print(' ');
+            if (i == 147) {
+                println();
+            }
+            println(it.next());
+        }
+    }
+}
