54a5b13af01ab66de9fa757b2eb092a05fd078be
guanquan.wang <wangguanquan@vipmro.net>
2019-07-22 17:05:02 +0800

    Fix#67 Rename some old 'cn.ttzero' package

--- src/main/java/org/ttzero/excel/reader/ExcelReader.java
+++ src/main/java/org/ttzero/excel/reader/ExcelReader.java
@@ -400,7 +400,7 @@ public class ExcelReader implements AutoCloseable {
                 break;
             case XLS:
                 try {
-                    Class<?> clazz = Class.forName("cn.ttzero.excel.reader.BIFF8Reader");
+                    Class<?> clazz = Class.forName("org.ttzero.excel.reader.BIFF8Reader");
                     Constructor<?> constructor = clazz.getDeclaredConstructor(Path.class, int.class, int.class);
                     er = (ExcelReader) constructor.newInstance(path, bufferSize, cacheSize);
                 } catch (Exception e) {
