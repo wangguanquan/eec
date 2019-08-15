439663bcc4a6cf5f3f53ffa2882abc4a0dc7b54e
guanquan.wang <wangguanquan@vipmro.net>
2019-08-15 08:42:05 +0800

    Add @Inherited annotation

--- src/main/java/org/ttzero/excel/annotation/IgnoreExport.java
+++ src/main/java/org/ttzero/excel/annotation/IgnoreExport.java
@@ -19,6 +19,7 @@ package org.ttzero.excel.annotation;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -30,6 +31,7 @@ import java.lang.annotation.Target;
  */
 @Target({ ElementType.FIELD, ElementType.METHOD })
 @Retention(RetentionPolicy.RUNTIME)
+@Inherited
 @Documented
 public @interface IgnoreExport {
     /**
--- src/main/java/org/ttzero/excel/annotation/IgnoreImport.java
+++ src/main/java/org/ttzero/excel/annotation/IgnoreImport.java
@@ -18,6 +18,7 @@ package org.ttzero.excel.annotation;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -29,6 +30,7 @@ import java.lang.annotation.Target;
  */
 @Target({ ElementType.FIELD, ElementType.METHOD })
 @Retention(RetentionPolicy.RUNTIME)
+@Inherited
 @Documented
 public @interface IgnoreImport {
     /**
