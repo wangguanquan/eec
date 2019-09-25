9ac06b3d80922645feffc71b66aab57ad3f130f6
wangguanquan <colvin@126.com>
2019-09-25 20:48:40 +0800

    Add filter test case

--- src/test/java/org/ttzero/excel/reader/ExcelReaderTest.java
+++ src/test/java/org/ttzero/excel/reader/ExcelReaderTest.java
@@ -28,6 +28,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.Iterator;
 
@@ -161,6 +162,19 @@ public class ExcelReaderTest {
                 print(row.getChar("VIP"));
                 println();
             });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test public void testFilter() {
+        try (ExcelReader reader = ExcelReader.read(testResourceRoot().resolve("1.xlsx"))) {
+            String[] games = reader.sheet(0)
+                .dataRows()
+                .map(row -> row.getString("游戏"))
+                .distinct()
+                .toArray(String[]::new);
+            print(Arrays.toString(games));
         } catch (IOException e) {
             e.printStackTrace();
         }
