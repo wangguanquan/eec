80163eaeca30da297917be700ee84b3ab0fd98d3
guanquan.wang <wangguanquan@vipmro.net>
2020-05-28 18:06:06 +0800

    简单实现备注功能

---
+++ src/main/java/org/ttzero/excel/entity/Comment.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2017-2020, guanquan.wang@yandex.com All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package org.ttzero.excel.entity;
+
+import java.util.function.Predicate;
+
+/**
+ * @author guanquan.wang at 2020-05-22 09:32
+ */
+public class Comment<T> {
+    private Predicate<T> predicate;
+    private String title;
+    private String value;
+
+    public Comment (Predicate<T> predicate, String title, String value) {
+        this.predicate = predicate;
+        this.title = title;
+        this.value = value;
+    }
+
+    public Predicate<T> getPredicate() {
+        return predicate;
+    }
+
+    public void setPredicate(Predicate<T> predicate) {
+        this.predicate = predicate;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
--- src/main/java/org/ttzero/excel/entity/e7/XMLWorkbookWriter.java
+++ src/main/java/org/ttzero/excel/entity/e7/XMLWorkbookWriter.java
@@ -22,6 +22,7 @@ import org.dom4j.Element;
 import org.dom4j.Namespace;
 import org.dom4j.QName;
 import org.ttzero.excel.annotation.TopNS;
+import org.ttzero.excel.entity.Comments;
 import org.ttzero.excel.entity.ExcelWriteException;
 import org.ttzero.excel.entity.IWorkbookWriter;
 import org.ttzero.excel.entity.IWorksheetWriter;
@@ -181,7 +182,16 @@ public class XMLWorkbookWriter implements IWorkbookWriter {
         }
 
         for (int i = 0; i < size; i++) {
-            contentType.add(new ContentType.Override(Const.ContentType.SHEET, "/xl/worksheets/sheet" + workbook.getSheetAt(i).getId() + Const.Suffix.XML));
+            Sheet sheet;
+            contentType.add(new ContentType.Override(Const.ContentType.SHEET
+                , "/xl/worksheets/sheet" + (sheet = workbook.getSheetAt(i)).getId() + Const.Suffix.XML));
+            Comments comments = sheet.getComments();
+            if (comments != null) {
+                comments.writeTo(root);
+                contentType.add(new ContentType.Override(Const.ContentType.COMMENTS
+                    , "/xl/comments" + sheet.getId() + Const.Suffix.XML));
+                contentType.add(new ContentType.Default(Const.ContentType.VMLDRAWING, "vml"));
+            }
         } // END
 
 
@@ -249,7 +259,7 @@ public class XMLWorkbookWriter implements IWorkbookWriter {
         }
         if (StringUtil.isEmpty(core.getTitle())) core.setTitle(workbook.getName());
         if (core.getCreated() == null) core.setCreated(new Date());
-        if (core.getCreated() == null) core.setModified(new Date());
+        if (core.getModified() == null) core.setModified(new Date());
 
         core.writeTo(root.getParent().resolve("docProps/core.xml"));
         contentType.add(new ContentType.Override(Const.ContentType.CORE, "/docProps/core.xml"));
@@ -302,7 +312,7 @@ public class XMLWorkbookWriter implements IWorkbookWriter {
             uris = topNs.uri();
             rootName = topNs.value();
             for (int i = 0; i < prefixs.length; i++) {
-                if (prefixs[i].length() == 0) { // 创建前缀为空的命名空间
+                if (prefixs[i].length() == 0) {
                     rootElement = factory.createElement(rootName, uris[i]);
                     break;
                 }
@@ -368,7 +378,8 @@ public class XMLWorkbookWriter implements IWorkbookWriter {
             }
             // Interlace changes color as default
             if (sheet.getAutoOdd() == 0) {
-                sheet.setOddFill(workbook.getOddFill() == null ? new Fill(PatternType.solid, new Color(226, 237, 218)) : workbook.getOddFill());
+                sheet.setOddFill(workbook.getOddFill() == null
+                    ? new Fill(PatternType.solid, new Color(226, 237, 218)) : workbook.getOddFill());
             }
             sheet.setId(i + 1);
             // default worksheet name
