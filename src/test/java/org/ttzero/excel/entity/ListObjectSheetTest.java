74b5f014645d3fc6835d54473fc0904b41ce09d7
guanquan.wang <guanquan.wang@yandex.com>
2022-07-23 14:45:00 +0800

    remove redundant references

--- src/test/java/org/ttzero/excel/entity/ListObjectSheetTest.java
+++ src/test/java/org/ttzero/excel/entity/ListObjectSheetTest.java
@@ -48,10 +48,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Date;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
