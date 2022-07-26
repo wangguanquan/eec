e2c526c68acacaa5d243eb63956aec92460c4f39
guanquan.wang <guanquan.wang@yandex.com>
2022-07-26 12:07:14 +0800

    Code style

--- src/main/java/org/ttzero/excel/entity/style/Styles.java
+++ src/main/java/org/ttzero/excel/entity/style/Styles.java
@@ -276,7 +276,7 @@ public class Styles implements Storable {
         int i = 0;
         for (Element e : sub) {
             String applyNumberFormat = getAttr(e, "applyNumberFormat");
-            if (isNotEmpty(applyNumberFormat) && ("1".equals(applyNumberFormat) || "true".equalsIgnoreCase(applyNumberFormat))){
+            if (isNotEmpty(applyNumberFormat) && ("1".equals(applyNumberFormat) || "true".equalsIgnoreCase(applyNumberFormat))) {
                 String numFmtId = getAttr(e, "numFmtId");
                 int style = Integer.parseInt(numFmtId) << INDEX_NUMBER_FORMAT;
                 self.map.put(i, style);
