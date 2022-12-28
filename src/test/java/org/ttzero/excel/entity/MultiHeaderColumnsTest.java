34650ca3c800c69b3633da6c2c543bb9bea3fcd6
guanquan.wang <guanquan.wang@yandex.com>
2022-12-28 10:13:48 +0800

    Test case

--- src/test/java/org/ttzero/excel/entity/MultiHeaderColumnsTest.java
+++ src/test/java/org/ttzero/excel/entity/MultiHeaderColumnsTest.java
@@ -26,22 +26,17 @@ import org.ttzero.excel.reader.ExcelReader;
 import org.ttzero.excel.entity.style.Font;
 import org.ttzero.excel.entity.style.Horizontals;
 import org.ttzero.excel.reader.HeaderRow;
-import org.ttzero.excel.entity.style.NumFmt;
 
 import java.awt.Color;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Timestamp;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
-import java.util.Map;
 
 /**
  * @author guanquan.wang at 2022-06-27 23:24
