794cbe11492a36f665ee2c49ded8d9855dff7ddc
guanquan.wang <guanquan.wang@yandex.com>
2023-01-18 11:06:02 +0800

    Adjust the theme color index

--- src/main/java/org/ttzero/excel/entity/style/ColorIndex.java
+++ src/main/java/org/ttzero/excel/entity/style/ColorIndex.java
@@ -40,8 +40,10 @@ public class ColorIndex {
 
     /** Defined 12 base theme color */
     public static final Color[] themeColors = {
+            // lt1, dk1
             new Color(255, 255, 255), new Color(  0,   0,   0),
-            new Color( 31,  73, 125), new Color(238, 236, 225),
+            // lt2, dk2
+            new Color(238, 236, 225), new Color( 31,  73, 125),
             new Color( 79, 129, 189), new Color(192,  80,  77),
             new Color(155, 187,  89), new Color(128, 100, 162),
             new Color( 75, 172, 198), new Color(247, 150,  70),
