9e15289ba9d7396cd5d29517542dbb0f47882196
guanquan.wang <guanquan.wang@yandex.com>
2023-02-04 23:56:32 +0800

    #330 Add test case

--- src/test/java/org/ttzero/excel/entity/TemplateTest.java
+++ src/test/java/org/ttzero/excel/entity/TemplateTest.java
@@ -17,17 +17,11 @@
 package org.ttzero.excel.entity;
 
 import org.junit.Test;
-import org.ttzero.excel.annotation.ExcelColumn;
-import org.ttzero.excel.annotation.FreezePanes;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import static org.ttzero.excel.reader.ExcelReaderTest.testResourceRoot;
@@ -56,45 +50,5 @@ public class TemplateTest extends WorkbookTest {
                 .withTemplate(fis, map)
                 .writeTo(defaultTestPath);
         }
-    }
-
-    @Test public void testAutoSize() throws IOException {
-        List<ServerReport> reports = new ArrayList<>(2);
-        for (int i = 0; i < 2; i++) {
-            ServerReport e = new ServerReport();
-            e.index = i + 1;
-            e.clientRequestNum = i;
-            e.name = "测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试";
-            e.duration = 12345L;
-            e.zipkinDt = "22-07-07";
-            e.timestamp = new Date();
-            reports.add(e);
-        }
-        new Workbook("服务数据")
-            .setAutoSize(true)
-            .addSheet(new ListSheet<>("服务报表1", reports)).writeTo(Paths.get("d://tmp/"));
-    }
-
-
-    @FreezePanes(topRow = 1)
-    public static class ServerReport {
-
-        @ExcelColumn(colIndex = 0, value = "序号")
-        private Integer index;
-
-        @ExcelColumn(colIndex = 2, value = "商户名")
-        private String name;
-
-        @ExcelColumn(colIndex = 1, value = "处理时间", hide = true, maxWidth = 254.88D)
-        private String zipkinDt;
-
-        @ExcelColumn(colIndex = 3, value = "时间", format = "yyyy-mm-dd hh:mm:ss")
-        private Date timestamp;
-
-        @ExcelColumn(colIndex = 4, value = "请求次数")
-        private Integer clientRequestNum;
-
-        @ExcelColumn(colIndex = 5, value = "持续时间")
-        private Long duration;
     }
 }
