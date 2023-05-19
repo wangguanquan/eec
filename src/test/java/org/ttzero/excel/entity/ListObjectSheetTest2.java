7cff6a980faaf4c72599b9fb5007c279292f696c
guanquan.wang <guanquan.wang@yandex.com>
2023-05-19 15:38:56 +0800

    Add test case

--- src/test/java/org/ttzero/excel/entity/ListObjectSheetTest2.java
+++ src/test/java/org/ttzero/excel/entity/ListObjectSheetTest2.java
@@ -18,7 +18,9 @@
 package org.ttzero.excel.entity;
 
 import org.junit.Test;
+import org.ttzero.excel.reader.Cell;
 import org.ttzero.excel.reader.ExcelReader;
+import org.ttzero.excel.reader.HeaderRow;
 
 import java.io.IOException;
 import java.util.List;
@@ -90,5 +92,85 @@ public class ListObjectSheetTest2 extends WorkbookTest {
             for (int i = 0, len = list.size(); i < len; i++)
                 assert list.get(i).equals(readList.get(i));
         }
+    }
+
+    @Test public void testSpecifyRowIgnoreHeaderWrite() throws IOException {
+        List<ListObjectSheetTest.Item> list = ListObjectSheetTest.Item.randomTestData();
+        new Workbook().setAutoSize(true)
+            .addSheet(new ListSheet<>(list).setStartRowIndex(4).ignoreHeader())
+            .writeTo(defaultTestPath.resolve("test specify row 4 ignore header ListSheet.xlsx"));
+
+        try (ExcelReader reader = ExcelReader.read(defaultTestPath.resolve("test specify row 4 ignore header ListSheet.xlsx"))) {
+            List<ListObjectSheetTest.Item> readList = reader.sheet(0)
+                .header(3)
+                .bind(ListObjectSheetTest.Item.class, new HeaderRow().with(createHeaderRow()))
+                .rows()
+                .map(row -> (ListObjectSheetTest.Item) row.get())
+                .collect(Collectors.toList());
+            assert list.size() == readList.size();
+            for (int i = 0, len = list.size(); i < len; i++)
+                assert list.get(i).equals(readList.get(i));
+        }
+    }
+
+    @Test public void testSpecifyRowStayA1IgnoreHeaderWrite() throws IOException {
+        List<ListObjectSheetTest.Item> list = ListObjectSheetTest.Item.randomTestData();
+        new Workbook().setAutoSize(true)
+            .addSheet(new ListSheet<>(list).setStartRowIndex(4, false).ignoreHeader())
+            .writeTo(defaultTestPath.resolve("test specify row 4 stay A1 ignore header ListSheet.xlsx"));
+
+        try (ExcelReader reader = ExcelReader.read(defaultTestPath.resolve("test specify row 4 stay A1 ignore header ListSheet.xlsx"))) {
+            List<ListObjectSheetTest.Item> readList = reader.sheet(0).bind(ListObjectSheetTest.Item.class, 4).rows().map(row -> (ListObjectSheetTest.Item) row.get()).collect(Collectors.toList());
+            assert list.size() == readList.size();
+            for (int i = 0, len = list.size(); i < len; i++)
+                assert list.get(i).equals(readList.get(i));
+        }
+    }
+
+    @Test public void testSpecifyRowAndColIgnoreHeaderWrite() throws IOException {
+        List<ListObjectSheetTest.Item> list = ListObjectSheetTest.Item.randomTestData(10);
+        new Workbook().setAutoSize(true)
+            .addSheet(new ListSheet<ListObjectSheetTest.Item>("Item"
+                , new Column("id", "id").setColIndex(3)
+                , new Column("name", "name").setColIndex(4))
+                .setData(list)
+                .setStartRowIndex(4)
+                .ignoreHeader()
+            ).writeTo(defaultTestPath.resolve("test specify row and cel ignore header ListSheet.xlsx"));
+
+        try (ExcelReader reader = ExcelReader.read(defaultTestPath.resolve("test specify row and cel ignore header ListSheet.xlsx"))) {
+            List<ListObjectSheetTest.Item> readList = reader.sheet(0).bind(ListObjectSheetTest.Item.class, 4).rows().map(row -> (ListObjectSheetTest.Item) row.get()).collect(Collectors.toList());
+            assert list.size() == readList.size();
+            for (int i = 0, len = list.size(); i < len; i++)
+                assert list.get(i).equals(readList.get(i));
+        }
+    }
+
+    @Test public void testSpecifyRowAndColStayA1IgnoreHeaderWrite() throws IOException {
+        List<ListObjectSheetTest.Item> list = ListObjectSheetTest.Item.randomTestData(10);
+        new Workbook().setAutoSize(true)
+            .addSheet(new ListSheet<ListObjectSheetTest.Item>("Item"
+                , new Column("id", "id").setColIndex(3)
+                , new Column("name", "name").setColIndex(4))
+                .setData(list)
+                .setStartRowIndex(4, false)
+                .ignoreHeader()
+            ).writeTo(defaultTestPath.resolve("test specify row and cel stay A1 ignore header ListSheet.xlsx"));
+
+        try (ExcelReader reader = ExcelReader.read(defaultTestPath.resolve("test specify row and cel stay A1 ignore header ListSheet.xlsx"))) {
+            List<ListObjectSheetTest.Item> readList = reader.sheet(0).bind(ListObjectSheetTest.Item.class, 4).rows().map(row -> (ListObjectSheetTest.Item) row.get()).collect(Collectors.toList());
+            assert list.size() == readList.size();
+            for (int i = 0, len = list.size(); i < len; i++)
+                assert list.get(i).equals(readList.get(i));
+        }
+    }
+
+    private static org.ttzero.excel.reader.Row createHeaderRow () {
+        org.ttzero.excel.reader.Row headerRow = new org.ttzero.excel.reader.Row() {};
+        Cell[] cells = new Cell[2];
+        cells[0] = new Cell((short) 1).setSv("id");
+        cells[1] = new Cell((short) 2).setSv("name");
+        headerRow.setCells(cells);
+        return headerRow;
     }
 }
