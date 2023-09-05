872941d1f0f8857740a4b0f9019ab784fc292c6d
guanquan.wang <guanquan.wang@yandex.com>
2023-09-05 20:51:37 +0800

    #358 add test case

--- src/test/java/org/ttzero/excel/reader/ExcelReaderTest2.java
+++ src/test/java/org/ttzero/excel/reader/ExcelReaderTest2.java
@@ -18,12 +18,17 @@
 package org.ttzero.excel.reader;
 
 import org.junit.Test;
+import org.ttzero.excel.entity.ListMapSheet;
+import org.ttzero.excel.entity.Workbook;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import static org.ttzero.excel.entity.WorkbookTest.defaultTestPath;
 import static org.ttzero.excel.reader.ExcelReaderTest.testResourceRoot;
 
 /**
@@ -58,4 +63,101 @@ public class ExcelReaderTest2 {
         }
     }
 
+    @Test public void testForceImport() throws IOException {
+        Map<String, Object> data1 = new HashMap<>();
+        data1.put("id", 1);
+        data1.put("name", "abc");
+
+        Map<String, Object> data2 = new HashMap<>();
+        data2.put("id", 2);
+        data2.put("name", "xyz");
+        new Workbook()
+            .addSheet(new ListMapSheet().setData(Arrays.asList(data1, data2)))
+            .writeTo(defaultTestPath.resolve("Force Import.xlsx"));
+
+        try (ExcelReader reader = ExcelReader.read(defaultTestPath.resolve("Force Import.xlsx"))) {
+            List<U> list = reader.sheet(0).forceImport().dataRows().map(row -> row.to(U.class)).collect(Collectors.toList());
+            assert list.size() == 2;
+            assert "1: abc".equals(list.get(0).toString());
+            assert "2: xyz".equals(list.get(1).toString());
+        }
+    }
+
+    @Test public void testUpperCaseRead() throws IOException {
+        Map<String, Object> data1 = new HashMap<>();
+        data1.put("ID", 1);
+        data1.put("NAME", "abc");
+
+        Map<String, Object> data2 = new HashMap<>();
+        data2.put("ID", 2);
+        data2.put("NAME", "xyz");
+
+        new Workbook()
+            .addSheet(new ListMapSheet(Arrays.asList(data1, data2)))
+            .writeTo(defaultTestPath.resolve("Upper case Reader test.xlsx"));
+
+        try (ExcelReader reader = ExcelReader.read(defaultTestPath.resolve("Upper case Reader test.xlsx"))) {
+            List<U> list = reader.sheet(0).forceImport().dataRows().map(row -> row.to(U.class)).collect(Collectors.toList());
+            assert list.size() == 2;
+            assert "0: null".equals(list.get(0).toString());
+            assert "0: null".equals(list.get(1).toString());
+
+            list = reader.sheet(0).reset().addHeaderColumnReadOption(HeaderRow.FORCE_IMPORT | HeaderRow.IGNORE_CASE)
+                .dataRows().map(row -> row.to(U.class)).collect(Collectors.toList());
+            assert list.size() == 2;
+            assert "1: abc".equals(list.get(0).toString());
+            assert "2: xyz".equals(list.get(1).toString());
+        }
+    }
+
+    @Test public void testCamelCaseRead() throws IOException {
+        Map<String, Object> data1 = new HashMap<>();
+        data1.put("USER_ID", 1);
+        data1.put("USER_NAME", "abc");
+
+        Map<String, Object> data2 = new HashMap<>();
+        data2.put("USER_ID", 2);
+        data2.put("USER_NAME", "xyz");
+
+        new Workbook()
+            .addSheet(new ListMapSheet(Arrays.asList(data1, data2)))
+            .writeTo(defaultTestPath.resolve("Underline case Reader test.xlsx"));
+
+        try (ExcelReader reader = ExcelReader.read(defaultTestPath.resolve("Underline case Reader test.xlsx"))) {
+            List<User> list = reader.sheet(0).forceImport().dataRows().map(row -> row.to(User.class)).collect(Collectors.toList());
+            assert list.size() == 2;
+            assert "0: null".equals(list.get(0).toString());
+            assert "0: null".equals(list.get(1).toString());
+
+            list = reader.sheet(0).reset().addHeaderColumnReadOption(HeaderRow.FORCE_IMPORT | HeaderRow.CAMEL_CASE)
+                .dataRows().map(row -> row.to(User.class)).collect(Collectors.toList());
+            assert list.size() == 2;
+            assert "1: abc".equals(list.get(0).toString());
+            assert "2: xyz".equals(list.get(1).toString());
+        }
+    }
+
+    public static class U {
+        int id;
+        String name;
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        @Override
+        public String toString() {
+            return id + ": " + name;
+        }
+    }
+
+    public static class User {
+        int userId;
+        String userName;
+
+        @Override
+        public String toString() {
+            return userId + ": " + userName;
+        }
+    }
 }
