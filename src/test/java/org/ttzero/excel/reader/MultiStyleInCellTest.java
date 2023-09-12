89a0dd8a2ff1cf9d0fe649c6d3c90059da36fc8d
guanquan.wang <guanquan.wang@yandex.com>
2023-09-12 12:01:59 +0800

    Add test case

--- src/test/java/org/ttzero/excel/reader/MultiStyleInCellTest.java
+++ src/test/java/org/ttzero/excel/reader/MultiStyleInCellTest.java
@@ -17,9 +17,10 @@
 package org.ttzero.excel.reader;
 
 import org.junit.Test;
-import org.ttzero.excel.Print;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Objects;
 
 import static org.ttzero.excel.reader.ExcelReaderTest.testResourceRoot;
 
@@ -29,8 +30,9 @@ import static org.ttzero.excel.reader.ExcelReaderTest.testResourceRoot;
 public class MultiStyleInCellTest {
     @Test public void testMulti() throws IOException {
         try (ExcelReader reader = ExcelReader.read(testResourceRoot().resolve("multi-style In cell.xlsx"))) {
-
-            reader.sheets().flatMap(Sheet::rows).forEach(Print::println);
+            String[] array = reader.sheet(0).rows().map(row -> row.getString(2)).filter(Objects::nonNull).toArray(String[]::new);
+            String[] expectArray = {"abred中文","IM8zc","B6tw","JWGv25V","fTY56z","nxWnqE","RyyJ8o","UgkTgnx","JDrYdU","Bl7Lh","w2vc9","4xbrwu","5A8RN","LUYhEG","y1Ee5Sl","fyM1","p6Dn","tMMWp","P1coCD","Ej2vXbZ","aUkcla","z2aLkN8","ljjaD8","r6T1hEP","qu5iO","TJt8C","sJlIYKN","Lt9NMp","D7bVWFk","1BwJHPP","S7Pf8hG","bHXsj49","EqXOeS","exyeBe","FtKjhf","KVs8LWt","uEkSmmj","1DxEoJ","tP3d","Nb2fu","nPJ6","2CrTk17","JWbwVD","aaX7p","NkFL","9j0xsxt","6rcGpax","h0blSId","gCPR","JO1rz","m5ZEpB3","WPpB77c","iEct","Tfuq","cHv9n3Y","TX9dhf","1Ueoj7","1EY9V","0ruA","XCXhJRp","uD36","Iat3fJE","1YsnAY","yrN9e","sSIM7G","gDXE","uamheu","ZXMN","8MymZBX","ic9aLbJ","e1QuI","bEQYyk","3DKbi0k","NRjW2","vz9HLE","aBYIMi","7Xe2lQz","d95SDS","jUfZ","ODrB2"};
+            assert Arrays.equals(expectArray, array);
         }
     }
 }
