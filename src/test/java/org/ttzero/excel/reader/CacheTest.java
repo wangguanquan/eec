18b68a5ff1cc2f40b421818d53529d956ed0aa0c
guanquan.wang <guanquan.wang@yandex.com>
2023-09-14 09:58:38 +0800

    Add test case

--- src/test/java/org/ttzero/excel/reader/CacheTest.java
+++ src/test/java/org/ttzero/excel/reader/CacheTest.java
@@ -18,9 +18,8 @@ package org.ttzero.excel.reader;
 
 import org.junit.Test;
 
-import java.util.Iterator;
-
-import static org.ttzero.excel.Print.println;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNull;
 
 /**
  * @author guanquan.wang at 2019-05-07 15:17
@@ -32,13 +31,14 @@ public class CacheTest {
         hot.put(2, "b");
         hot.put(3, "c");
         hot.put(4, "d");
+        assert "4:d=>3:c=>2:b=>1:a".equals(hot.toString());
 
         assert hot.size() == 4;
         assert hot.get(2).equals("b");
-
-        hot.forEach(e -> println(e.getKey() + ": " + e.getValue()));
-
-        println(hot.get(3));
+        assert "2:b=>4:d=>3:c=>1:a".equals(hot.toString());
+        assert hot.get(4).equals("d");
+        assert "4:d=>2:b=>3:c=>1:a".equals(hot.toString());
+        System.out.println();
     }
 
     @Test public void testPut2() {
@@ -49,11 +49,11 @@ public class CacheTest {
         hot.put("c", 3);
         hot.put("d", 4);
         hot.put("e", 5);
+        assert "e:5=>d:4=>c:3=>a:8=>b:2".equals(hot.toString());
 
         assert hot.size() == 5;
         assert hot.get("c") == 3;
-
-        hot.forEach(e -> println(e.getKey() + ": " + e.getValue()));
+        assert "c:3=>e:5=>d:4=>a:8=>b:2".equals(hot.toString());
     }
 
     @Test public void testIterator() {
@@ -64,10 +64,51 @@ public class CacheTest {
         hot.put("d", 4);
         hot.put("e", 5);
 
-        for (Iterator<Cache.Entry<String, Integer>> ite = hot.iterator(); ite.hasNext();) {
-            Cache.Entry<String, Integer> e = ite.next();
-            println(e.getKey() + ": " + e.getValue());
+        String[] expected = {"e:5", "d:4", "c:3", "b:2", "a:1"};
+        int i = 0;
+        for (Cache.Entry<String, Integer> e : hot) {
+            assert expected[i++].equals(e.toString());
         }
+    }
+
+    @Test public void testRemoveTail() {
+        FixSizeLRUCache<Integer, String> hot = FixSizeLRUCache.create();
+        hot.put(1, "a");
+        hot.put(2, "b");
+        hot.put(3, "c");
+        hot.put(4, "d");
+
+        assert hot.size() == 4;
+        assert hot.get(1).equals("a");
+        assert "1:a=>4:d=>3:c=>2:b".equals(hot.toString());
+        assertNotEquals(hot.get(2), "B");
+        assert "2:b=>1:a=>4:d=>3:c".equals(hot.toString());
+
+        hot.remove();
+
+        assert "2:b=>1:a=>4:d".equals(hot.toString());
+        assert hot.size() == 3;
+        assert hot.get(1).equals("a");
+        assert "1:a=>2:b=>4:d".equals(hot.toString());
+
+        hot.remove();
+        assert "1:a=>2:b".equals(hot.toString());
+        assert hot.size() == 2;
+        assert hot.get(2).equals("b");
+        assert "2:b=>1:a".equals(hot.toString());
+        hot.remove();
+        assert "2:b".equals(hot.toString());
+        assert hot.size() == 1;
+        assertNull(hot.get(3));
+        hot.remove();
+        assert "".equals(hot.toString());
+        assert hot.size() == 0;
+        assertNull(hot.get(4));
+        hot.remove();
+        hot.remove();
+        hot.remove();
+        hot.remove();
+        assert hot.size() == 0;
     }
 
     @Test public void testRemove() {
@@ -77,24 +118,21 @@ public class CacheTest {
         hot.put(3, "c");
         hot.put(4, "d");
 
-        assert hot.size() == 4;
-        hot.forEach(e -> println(e.getKey() + ": " + e.getValue()));
+        assert "b".equals(hot.remove(2));
+        assert "4:d=>3:c=>1:a".equals(hot.toString());
 
-        hot.remove();
+        assert "a".equals(hot.remove(1));
+        assert "4:d=>3:c".equals(hot.toString());
 
-        assert hot.size() == 3;
+        assert "d".equals(hot.remove(4));
+        assert "3:c".equals(hot.toString());
 
-        hot.remove();
-        hot.forEach(e -> println(e.getKey() + ": " + e.getValue()));
-        hot.remove();
-        hot.forEach(e -> println(e.getKey() + ": " + e.getValue()));
-        hot.remove();
-        hot.forEach(e -> println(e.getKey() + ": " + e.getValue()));
-        hot.remove();
-        hot.remove();
-        hot.remove();
-        hot.remove();
-        hot.forEach(e -> println(e.getKey() + ": " + e.getValue()));
+        assertNull(hot.remove(4));
+        assert "3:c".equals(hot.toString());
+
+        assert "c".equals(hot.remove(3));
+        assert hot.size() == 0;
+        assert "".equals(hot.toString());
     }
 
     @Test public void testClear() {
@@ -108,7 +146,6 @@ public class CacheTest {
         hot.clear();
 
         assert hot.size() == 0;
-        hot.forEach(e -> println(e.getKey() + ": " + e.getValue()));
     }
 
     @Test public void testRemoveAndAdd() {
@@ -116,9 +153,14 @@ public class CacheTest {
         cache.put("a", 1);
         cache.put("b", 2);
 
-        cache.remove("a");
-        cache.put("a", 5);
+        assert cache.size() == 2;
+        assert cache.get("a") == 1;
+        assert cache.get("b") == 2;
 
-        cache.forEach(e -> println(e.getKey() + ": " + e.getValue()));
+        cache.remove("a");
+        assert cache.size() == 1;
+        cache.put("a", 5);
+        assert cache.size() == 2;
+        assert cache.get("a") == 5;
     }
 }
