e3fe57a386dfd4124132929aa1226c286416647d
guanquan.wang <guanquan.wang@yandex.com>
2023-09-22 22:10:26 +0800

    Add assert case

--- src/test/java/org/ttzero/excel/entity/GridLinesTest.java
+++ src/test/java/org/ttzero/excel/entity/GridLinesTest.java
@@ -19,6 +19,10 @@ package org.ttzero.excel.entity;
 
 import org.junit.Test;
 import org.ttzero.excel.entity.e7.XMLWorksheetWriter;
+import org.ttzero.excel.reader.ExcelReader;
+import org.ttzero.excel.reader.HeaderRow;
+import org.ttzero.excel.reader.Row;
+import org.ttzero.excel.reader.Sheet;
 
 import java.io.IOException;
 import java.sql.Connection;
@@ -26,8 +30,10 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
+import java.util.Iterator;
 import java.util.List;
-import java.util.function.Supplier;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import static org.ttzero.excel.entity.ListMapSheetTest.createTestData;
 
@@ -36,68 +42,154 @@ import static org.ttzero.excel.entity.ListMapSheetTest.createTestData;
  */
 public class GridLinesTest extends SQLWorkbookTest {
     @Test public void testListSheet() throws IOException {
-        new Workbook("ListSheet ignore grid lines")
-            .addSheet(new ListSheet<>(ListObjectSheetTest.Item.randomTestData()).hideGridLines())
-            .writeTo(defaultTestPath);
+        String fileName = "ListSheet ignore grid lines.xlsx";
+        List<ListObjectSheetTest.Item> expectList = ListObjectSheetTest.Item.randomTestData();
+        new Workbook()
+            .addSheet(new ListSheet<>(expectList).hideGridLines())
+            .writeTo(defaultTestPath.resolve(fileName));
+
+        try (ExcelReader reader = ExcelReader.read(defaultTestPath.resolve(fileName))) {
+            List<ListObjectSheetTest.Item> list = reader.sheet(0).bind(ListObjectSheetTest.Item.class, 1).rows().map(row -> (ListObjectSheetTest.Item) row.get()).collect(Collectors.toList());
+            assert expectList.size() == list.size();
+            for (int i = 0, len = expectList.size(); i < len; i++) {
+                ListObjectSheetTest.Item expect = expectList.get(i), e = list.get(i);
+                assert expect.equals(e);
+            }
+        }
     }
 
     @Test public void testListSheetPaging() throws IOException {
-        new Workbook("ListSheet Paging ignore grid lines")
+        String fileName = "ListSheet Paging ignore grid lines.xlsx";
+        List<ListObjectSheetTest.Item> expectList = ListObjectSheetTest.Item.randomTestData();
+        Workbook workbook = new Workbook()
             .setAutoSize(true)
-            .addSheet(new ListSheet<>(ListObjectSheetTest.Item.randomTestData()).hideGridLines()
+            .addSheet(new ListSheet<>(expectList).hideGridLines()
                 .setSheetWriter(new XMLWorksheetWriter() {
                 @Override
                 public int getRowLimit() {
                     return 10;
                 }
-            }))
-            .writeTo(defaultTestPath);
+            }));
+        workbook.writeTo(defaultTestPath.resolve(fileName));
+
+        int count = expectList.size(), rowLimit = workbook.getSheetAt(0).getSheetWriter().getRowLimit();
+        try (ExcelReader reader = ExcelReader.read(defaultTestPath.resolve(fileName))) {
+            assert reader.getSize() == (count % (rowLimit - 1) > 0 ? count / (rowLimit - 1) + 1 : count / (rowLimit - 1)); // Include header row
+
+            for (int i = 0, len = reader.getSize(), a = 0; i < len; i++) {
+                Sheet sheet = reader.sheet(i).header(1).bind(ListObjectSheetTest.Item.class);
+                org.ttzero.excel.reader.HeaderRow header = (HeaderRow) sheet.getHeader();
+                assert "id".equals(header.get(0));
+                assert "name".equals(header.get(1));
+                Iterator<Row> iter = sheet.iterator();
+                while (iter.hasNext()) {
+                    ListObjectSheetTest.Item expect = expectList.get(a++), e = iter.next().get();
+                    assert expect.equals(e);
+                }
+            }
+        }
     }
 
     @Test public void testListMapSheet() throws IOException {
-        new Workbook("ListMapSheet ignore grid lines")
-            .addSheet(new ListMapSheet(createTestData()).hideGridLines())
-            .writeTo(defaultTestPath);
+        String fileName = "ListMapSheet ignore grid lines.xlsx";
+        List<Map<String, ?>> expectList = createTestData();
+        new Workbook()
+            .addSheet(new ListMapSheet(expectList).hideGridLines())
+            .writeTo(defaultTestPath.resolve(fileName));
+
+        try (ExcelReader reader = ExcelReader.read(defaultTestPath.resolve(fileName))) {
+            List<Map<String, ?>> list = reader.sheet(0).dataRows().map(Row::toMap).collect(Collectors.toList());
+            assert expectList.size() == list.size();
+            for (int i = 0, len = expectList.size(); i < len; i++) {
+                Map<String, ?> expect = expectList.get(i), e = list.get(i);
+                assert expect.equals(e);
+            }
+        }
     }
 
     @Test public void testStatementSheet() throws SQLException, IOException {
+        String fileName = "StatementSheet ignore grid lines.xlsx";
+        String sql = "select id, name, age, create_date, update_date from student order by age";
         try (Connection con = getConnection()) {
-            new Workbook("StatementSheet ignore grid lines")
-                .addSheet(new StatementSheet(con, "select id, name, age, create_date, update_date from student order by age"
+            new Workbook()
+                .addSheet(new StatementSheet(con, sql
                     , new Column("学号", int.class)
-                    , new Column("性名", String.class)
+                    , new Column("姓名", String.class)
                     , new Column("年龄", int.class)
                     , new Column("创建时间", Timestamp.class).setColIndex(0)
                     , new Column("更新", Timestamp.class)
-                ).hideGridLines()).writeTo(defaultTestPath);
+                ).hideGridLines()).writeTo(defaultTestPath.resolve(fileName));
+
+            PreparedStatement ps = con.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            try (ExcelReader reader = ExcelReader.read(defaultTestPath.resolve(fileName))) {
+                Iterator<org.ttzero.excel.reader.Row> iter = reader.sheet(0).iterator();
+                assert iter.hasNext();
+                org.ttzero.excel.reader.Row header = iter.next();
+                assert "创建时间".equals(header.getString(0));
+                assert "学号".equals(header.getString(1));
+                assert "姓名".equals(header.getString(2));
+                assert "年龄".equals(header.getString(3));
+                assert "更新".equals(header.getString(4));
+
+                while (rs.next()) {
+                    assert iter.hasNext();
+                    org.ttzero.excel.reader.Row row = iter.next();
+
+                    assert rs.getTimestamp(4) != null ? rs.getTimestamp(4).getTime() / 1000 == row.getTimestamp(0).getTime() / 1000 : row.getTimestamp(0) == null;
+                    assert rs.getInt(1) == row.getInt(1);
+                    assert rs.getString(2).equals(row.getString(2));
+                    assert rs.getInt(3) == row.getInt(3);
+                    assert rs.getTimestamp(5) != null ? rs.getTimestamp(5).getTime() / 1000 == row.getTimestamp(4).getTime() / 1000 : row.getTimestamp(4) == null;
+                }
+            }
+            rs.close();
+            ps.close();
         }
     }
 
     @Test public void testResultSetSheet() throws SQLException, IOException {
+        String fileName = "ResultSetSheet ignore grid lines.xlsx";
+        String sql = "select id, name, age, create_date, update_date from student limit 10";
         try (
             Connection con = getConnection();
-            PreparedStatement ps = con.prepareStatement("select id, name, age, create_date, update_date from student limit 10");
+            PreparedStatement ps = con.prepareStatement(sql);
             ResultSet rs = ps.executeQuery()
         ) {
-            new Workbook("ResultSetSheet ignore grid lines")
+            new Workbook()
                 .addSheet(new ResultSetSheet(rs
                     , new Column("学号", int.class)
-                    , new Column("性名", String.class)
+                    , new Column("姓名", String.class)
                     , new Column("年龄", Integer.class)
                     , new Column("创建时间", Timestamp.class)
                     , new Column("更新", Timestamp.class)
-                ).hideGridLines()).writeTo(defaultTestPath);
-        }
-    }
+                ).hideGridLines()).writeTo(defaultTestPath.resolve(fileName));
 
-    public static class HideGridLineAllType extends ListObjectSheetTest.AllType {
-        public static List<ListObjectSheetTest.AllType> randomTestData() {
-            return randomTestData(HideGridLineAllType::new);
-        }
+            PreparedStatement ps1 = con.prepareStatement(sql);
+            ResultSet rs1 = ps.executeQuery();
+            try (ExcelReader reader = ExcelReader.read(defaultTestPath.resolve(fileName))) {
+                Iterator<org.ttzero.excel.reader.Row> iter = reader.sheet(0).iterator();
+                assert iter.hasNext();
+                org.ttzero.excel.reader.Row header = iter.next();
+                assert "学号".equals(header.getString(0));
+                assert "姓名".equals(header.getString(1));
+                assert "年龄".equals(header.getString(2));
+                assert "创建时间".equals(header.getString(3));
+                assert "更新".equals(header.getString(4));
 
-        public static List<ListObjectSheetTest.AllType> randomTestData(Supplier<ListObjectSheetTest.AllType> sup) {
-            int size = random.nextInt(100) + 1;
-            return randomTestData(size, sup);
+                while (rs1.next()) {
+                    assert iter.hasNext();
+                    org.ttzero.excel.reader.Row row = iter.next();
+
+                    assert rs1.getInt(1) == row.getInt(0);
+                    assert rs1.getString(2).equals(row.getString(1));
+                    assert rs1.getInt(3) == row.getInt(2);
+                    assert rs1.getTimestamp(4) != null ? rs.getTimestamp(4).getTime() / 1000 == row.getTimestamp(3).getTime() / 1000 : row.getTimestamp(3) == null;
+                    assert rs1.getTimestamp(5) != null ? rs.getTimestamp(5).getTime() / 1000 == row.getTimestamp(4).getTime() / 1000 : row.getTimestamp(4) == null;
+                }
+            }
+            rs1.close();
+            ps1.close();
         }
     }
 }
