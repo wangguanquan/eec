1c2af3070cdf14f608934ad9ddf22e195793fa56
guanquan.wang <guanquan.wang@yandex.com>
2023-10-21 20:42:00 +0800

    Java doc汉化

--- src/main/java/org/ttzero/excel/annotation/StyleDesign.java
+++ src/main/java/org/ttzero/excel/annotation/StyleDesign.java
@@ -26,8 +26,16 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Customize Style design
+ * 动态样式处理器
  *
+ * <p>动态样式是指根据行数据为每个单元格或整行设置不同样式，这个功能可以极大丰富文件的可读性和多样性.
+ * StyleDesign注解作用于{@code type}类时修改整行样式，作用于{@code field}和{@code method}
+ *  时修改单个单元格样式。StyleDesign指定的样式处理器必须实现{@link StyleProcessor}接口，</p>
+ *
+ * <p>参考文档:</p>
+ * <p><a href="https://github.com/wangguanquan/eec/wiki/4-%E5%8A%A8%E6%80%81%E8%AE%BE%E7%BD%AE%E6%A0%B7%E5%BC%8F">动态设置样式</a></p>
+ *
+ * @see StyleProcessor
  * @author suyl at 2022-03-23 17:38
  *
  */
@@ -36,9 +44,9 @@ import java.lang.annotation.Target;
 @Inherited
 public @interface StyleDesign {
     /**
-     * Specify a {@link StyleProcessor} to setting the cell style
+     * 指定动态样式处理器
      *
-     * @return a {@link StyleProcessor} class
+     * @return 样式处理器
      */
     Class<? extends StyleProcessor> using() default StyleProcessor.None.class;
 }
