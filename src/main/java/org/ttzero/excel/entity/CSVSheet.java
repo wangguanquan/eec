e0c896420a34e2964cf7e111aeac9460579c3052
guanquan.wang <guanquan.wang@yandex.com>
2023-11-05 11:01:01 +0800

    Doc汉化

--- src/main/java/org/ttzero/excel/entity/CSVSheet.java
+++ src/main/java/org/ttzero/excel/entity/CSVSheet.java
@@ -32,35 +32,54 @@ import java.nio.file.StandardCopyOption;
 import static org.ttzero.excel.util.FileUtil.exists;
 
 /**
+ * {@code CSVSheet}的数据源为csv文件，可用于将csv文件转为Excel工作表，
+ * 通过{@link CSVUtil}提供的迭代器使得{@code CSVSheet}与{@code ListSheet}工作表具有
+ * 相似的切片属性，输出协议调用{@code nextBlock}获取分片数据时{@code CSVSheet}从CSVIterator
+ * 中逐行读取数据并输出以此控制整个过程对内存的消耗
+ *
  * @author guanquan.wang at 2019-09-26 08:33
  */
 public class CSVSheet extends Sheet {
 
-    // The csv source file path
+    /**
+     * csv文件路径
+     */
     private Path path;
+    /**
+     * csv行迭代器，配合工作表输出协议获取数据可以极大降低内存消耗
+     */
     private CSVUtil.RowsIterator iterator;
+    /**
+     * 是否需要清理临时资源，实例化时如果传入{@code InputStream}或{@code Reader}时会先将数据保存到临时文件
+     * 然后创建迭代器逐行读取数据，这个过程产生的临时文件会在关闭工作表时被一起清理
+     */
     private boolean shouldClean;
+    /**
+     * 是否包含表头，如果csv文件无表头时可将此值置为{@code false}
+     */
     private boolean hasHeader;
 
     /**
-     * Constructor worksheet
+     * 实例化工作表，未指定工作表名称时默认以{@code 'Sheet'+id}命名
      */
     public CSVSheet() {
         super();
     }
 
     /**
-     * Constructor worksheet
-     * @param path the csv source file path
+     * 实例化工作表并指定csv文件路径
+     *
+     * @param path csv文件路径
      */
     public CSVSheet(Path path) {
         this.path = path;
     }
 
     /**
-     * Constructor worksheet
-     * @param name the worksheet name
-     * @param path the csv source file path
+     * 实例化工作表并指定工作表名和csv文件路径
+     *
+     * @param name 工作表名
+     * @param path csv文件路径
      */
     public CSVSheet(String name, Path path) {
         super(name);
@@ -68,8 +87,9 @@ public class CSVSheet extends Sheet {
     }
 
     /**
-     * Constructor worksheet
-     * @param is the csv source InputStream
+     * 实例化工作表并指定csv文件字节流
+     *
+     * @param is csv文件字节流
      * @throws IOException if I/O error occur.
      */
     public CSVSheet(InputStream is) throws IOException {
@@ -77,9 +97,10 @@ public class CSVSheet extends Sheet {
     }
 
     /**
-     * Constructor worksheet
-     * @param name the worksheet name
-     * @param is the csv source InputStream
+     * 实例化工作表并指定工作表名和csv文件字节流
+     *
+     * @param name 工作表名
+     * @param is csv文件字节流
      * @throws IOException if I/O error occur.
      */
     public CSVSheet(String name, InputStream is) throws IOException {
@@ -90,8 +111,9 @@ public class CSVSheet extends Sheet {
     }
 
     /**
-     * Constructor worksheet
-     * @param reader the csv source InputStream
+     * 实例化工作表并指定csv文件字符流
+     *
+     * @param reader csv文件字符流
      * @throws IOException if I/O error occur.
      */
     public CSVSheet(Reader reader) throws IOException {
@@ -99,9 +121,10 @@ public class CSVSheet extends Sheet {
     }
 
     /**
-     * Constructor worksheet
-     * @param name the worksheet name
-     * @param reader the csv source InputStream
+     * 实例化工作表并指定工作表名和csv文件字符流
+     *
+     * @param name 工作表名
+     * @param reader csv文件字符流
      * @throws IOException if I/O error occur.
      */
     public CSVSheet(String name, Reader reader) throws IOException {
@@ -118,10 +141,10 @@ public class CSVSheet extends Sheet {
     }
 
     /**
-     * Setting header flag
+     * 设置是否需要表头
      *
-     * @param hasHeader boolean value
-     * @return {@link CSVSheet}
+     * @param hasHeader true: 包含表头
+     * @return 当前工作表
      */
     public CSVSheet setHasHeader(boolean hasHeader) {
         this.hasHeader = hasHeader;
@@ -129,7 +152,7 @@ public class CSVSheet extends Sheet {
     }
 
     /**
-     * Release resources
+     * 清理临时文件
      *
      * @throws IOException if I/O error occur
      */
@@ -149,7 +172,8 @@ public class CSVSheet extends Sheet {
     }
 
     /**
-     * Reset the row-block data
+     * 重置{@code RowBlock}行块数据，使用csv行迭代器逐行读取数据并重置行块，由于csv格式并不包含任务样式
+     * 所以{@code CSVSheet}并不支持任务样式设定
      */
     @Override
     protected void resetBlockData() {
@@ -223,14 +247,14 @@ public class CSVSheet extends Sheet {
 //        }
 //        return !hasHeader;
 //    }
-
-    /**
-     * The Worksheet row limit
-     *
-     * @return the limit
-     */
-    @Override
-    protected int getRowLimit() {
-        return Integer.MAX_VALUE;
-    }
+//
+//    /**
+//     * The Worksheet row limit
+//     *
+//     * @return the limit
+//     */
+//    @Override
+//    protected int getRowLimit() {
+//        return Integer.MAX_VALUE;
+//    }
 }
