2accce1cc48b0f3e412bc404086a6f7d75ae7fae
guanquan.wang <guanquan.wang@hotmail.com>
2024-07-16 19:44:42 +0800

    删除无效代码

--- src/main/java/org/ttzero/excel/entity/e7/XMLCellValueAndStyle.java
+++ src/main/java/org/ttzero/excel/entity/e7/XMLCellValueAndStyle.java
@@ -21,29 +21,4 @@ import org.ttzero.excel.entity.ICellValueAndStyle;
 /**
  * @author guanquan.wang at 2019-09-25 11:25
  */
-public class XMLCellValueAndStyle implements ICellValueAndStyle {
-
-//    /**
-//     * Int value conversion to others
-//     *
-//     * @param row  the row number
-//     * @param cell the cell
-//     * @param o    the cell value
-//     * @param hc   the header column
-//     */
-//    @Override
-//    public void conversion(Row row, Cell cell, Object o, Column hc) {
-//        Object e = hc.getConversion().conversion(o);
-//        if (e != null) {
-//            Class<?> clazz = e.getClass();
-//            setCellValue(row, cell, e, hc, clazz, false);
-//            // FIXME 转转换后是否根据转换后的类型重新设置对齐不明确，暂时不重置
-////            int style = hc.getCellStyle(clazz);
-//            cell.xf = getStyleIndex(row, hc, o, hc.getCellStyle());
-//        } else {
-//            cell.blank();
-//            cell.xf = getStyleIndex(row, hc, null, hc.getCellStyle());
-//        }
-//    }
-
-}
+public class XMLCellValueAndStyle implements ICellValueAndStyle { }
