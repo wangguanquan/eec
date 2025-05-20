defb0db06558144f4fc8311628c15ff98bd9d617
Chai <chaijp@reflynet.com>
2025-05-20 14:00:16 +0800

    添加rootfield支持

--- src/main/java/org/ttzero/excel/reader/NestedHeaderRow.java
+++ src/main/java/org/ttzero/excel/reader/NestedHeaderRow.java
@@ -6,10 +6,12 @@ import org.ttzero.excel.util.StringUtil;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.ttzero.excel.util.ReflectUtil.listDeclaredFieldsUntilJavaPackage;
 
@@ -30,12 +32,18 @@ public class NestedHeaderRow extends HeaderRow {
     private Map<Field, NestedHeaderRow> nestedColumnMap;
     private NestedObject nestedObject;
     private String columnNameFormat;
+    private Field rootField;
 
     public NestedHeaderRow() {
     }
 
     public NestedHeaderRow(NestedObject nestedObject) {
+        this(nestedObject, null);
+    }
+
+    public NestedHeaderRow(NestedObject nestedObject, Field rootField) {
         this.nestedObject = nestedObject;
+        this.rootField = rootField;
         setColumnNameFormat(nestedObject.columnNameFormat());
     }
 
@@ -82,13 +90,16 @@ public class NestedHeaderRow extends HeaderRow {
             for (Field f : declaredFields) {
                 if (!f.isAccessible()) f.setAccessible(true);
                 NestedObject object = f.getAnnotation(NestedObject.class);
-                NestedHeaderRow headerRow = new NestedHeaderRow(object);
+                NestedHeaderRow headerRow = new NestedHeaderRow(object, f);
                 headerRow.with(this).setOptions(option << 16 >>> 16);
                 // 拼接父类列名格式化规则
-                if (object.columnNameFormatExtend()) {
-                    headerRow.setColumnNameFormat(columnNameFormat);
-                }
+                if (object.columnNameFormatExtend()) headerRow.setColumnNameFormat(columnNameFormat);
                 headerRow.setClass(f.getType());
+                for (ListSheet.EntryColumn column : headerRow.columns) {
+                    if (rootField != null) column.addFirstRootField(rootField);
+                    column.addFirstRootField(f);
+                    column.setName(String.format(headerRow.columnNameFormat, column.getName()));
+                }
                 nestedColumnMap.put(f, headerRow);
             }
         }
@@ -102,6 +113,27 @@ public class NestedHeaderRow extends HeaderRow {
             }
         }
         return this;
+    }
+
+    /**
+     * 获取所有列信息（包括嵌套对象中的列）
+     * @return 列信息
+     */
+    protected ListSheet.EntryColumn[] getAllColumns() {
+        ListSheet.EntryColumn[] columns = this.columns;
+        if (nestedColumnMap == null || nestedColumnMap.isEmpty()) {
+            return columns;
+        }
+        List<ListSheet.EntryColumn> nestedColumnList = nestedColumnMap.values().stream()
+                .map(NestedHeaderRow::getAllColumns)
+                .flatMap(Stream::of)
+                .collect(Collectors.toList());
+        if (columns == null) {
+            return nestedColumnList.toArray(new ListSheet.EntryColumn[0]);
+        }
+        nestedColumnList.addAll(Stream.of(columns).collect(Collectors.toList()));
+        nestedColumnList.sort(Comparator.comparingInt(column -> column.colIndex));
+        return nestedColumnList.toArray(new ListSheet.EntryColumn[0]);
     }
 
     /**
